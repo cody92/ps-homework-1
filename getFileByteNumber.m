02c23775225a904b11541e07f0207a51b837bd6c
Ionut Codreanu <codydoc92@gmail.com>
2014-10-19 11:32:20 +0300

    Add getFileByteNumber function to return number of bytes and bites for a file

---
+++ getFileByteNumber.m
@@ -0,0 +1,12 @@
+function [byteNumber bitNumber] = getFileByteNumber( fileName )
+
+
+	fileID = fopen(fileName);
+
+	fileInfo = dir(fileName);
+	byteNumber = fileInfo.bytes;
+
+	[file bitNumber] = fread(fileID,inf,'bit1');
+	fclose(fileID);
+end
+
