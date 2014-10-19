d727b52153a0f2f1f2ba113f3faf443d959b71f8
Ionut Codreanu <codydoc92@gmail.com>
2014-10-19 19:41:05 +0300

    Last updates

--- homework1.m
+++ homework1.m
@@ -2,7 +2,7 @@ function homework1()
 	files = getSampleFiles();
 	for i=1:length(files)
 		[bytes bits] = getFileByteNumber(files{i});
-		disp(sprintf('Fisierul: %s are un numar de bytes de %d si un numar de biti de %d. Raportul bits/bytes este %1.f\n', files{i},bytes,bits,bits/bytes));
+		disp(sprintf('Fisierul: %s are un numar de %d bytes si un numar de %d biti. Raportul bits/bytes este %1.f\n', files{i},bytes,bits,bits/bytes));
 	end
 end
 
