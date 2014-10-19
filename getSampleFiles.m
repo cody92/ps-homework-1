4646ea1d47e555ee1b19a9db9a0de3e6b8f71397
Ionut Codreanu <codydoc92@gmail.com>
2014-10-19 10:11:06 +0300

    Add getSampleFiles function to get a list with sample files

---
+++ getSampleFiles.m
@@ -0,0 +1,20 @@
+function [sample_files] = getSampleFiles()
+
+	DATA_FOLDER = 'data';
+
+	strArray = java_array('java.lang.String', 20);
+	list_folders = dir(DATA_FOLDER);
+	folders_number = length(list_folders)
+	sample_files_number = 1;
+	fileSep = filesep;
+	for i=3:folders_number
+		childFiles = dir(strcat(DATA_FOLDER,fileSep,list_folders(i).name));
+		
+		for j=3:length(childFiles)
+			strArray(sample_files_number) = java.lang.String(strcat(DATA_FOLDER,fileSep,list_folders(i).name,fileSep,childFiles(j).name))
+			sample_files_number = sample_files_number + 1;
+		end
+	end
+	sample_files = cell(strArray);
+end
+
