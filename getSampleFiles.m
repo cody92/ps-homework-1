706b99056c248d53120c6f4c24e953519386a458
Ionut Codreanu <codydoc92@gmail.com>
2014-10-19 12:21:08 +0300

    Add homework 1 subpoint to show bytes number and bits number for every file from sample data files

--- getSampleFiles.m
+++ getSampleFiles.m
@@ -2,16 +2,16 @@ function [sample_files] = getSampleFiles()
 
 	DATA_FOLDER = 'data';
 
-	strArray = java_array('java.lang.String', 20);
+	strArray = java_array('java.lang.String', 12);
 	list_folders = dir(DATA_FOLDER);
-	folders_number = length(list_folders)
+	folders_number = length(list_folders);
 	sample_files_number = 1;
 	fileSep = filesep;
 	for i=3:folders_number
 		childFiles = dir(strcat(DATA_FOLDER,fileSep,list_folders(i).name));
 		
 		for j=3:length(childFiles)
-			strArray(sample_files_number) = java.lang.String(strcat(DATA_FOLDER,fileSep,list_folders(i).name,fileSep,childFiles(j).name))
+			strArray(sample_files_number) = java.lang.String(strcat(DATA_FOLDER,fileSep,list_folders(i).name,fileSep,childFiles(j).name));
 			sample_files_number = sample_files_number + 1;
 		end
 	end
