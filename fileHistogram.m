128841740d101b38e70093eab348b3c775f9a8dd
Ionut Codreanu <codydoc92@gmail.com>
2014-10-21 00:19:22 +0300

    Histograms updates

--- fileHistogram.m
+++ fileHistogram.m
@@ -2,11 +2,12 @@ function fileHistogram(fileName)
 
 disp(sprintf('Histogramele pentru fisierul: %s',fileName));
 
-figure('Name', horzcat('Histograma simboluri 8 biti a fisierului ', fileName));
+figure('Name', horzcat('Histograme ale a fisierului ', fileName));
 fileContent8 = readFile(fileName,'uint8');
+subplot(2,1,1);
 hist(fileContent8,0:255);
 
-figure('Name', horzcat('Histograma simboluri 16 biti a fisierului ', fileName));
+subplot(2,1,2);
 fileContent16 = readFile(fileName,'uint16');
 hist(fileContent16,0:65535);
 
